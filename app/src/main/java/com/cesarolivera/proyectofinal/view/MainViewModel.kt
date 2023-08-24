8ecf4737133983469e8d25f66f6bc27899305efc
Cesar Olivera <cesar@mypcomputacion.com>
2023-08-24 16:48:28 -0300

    Eliminando lineas comentadas de otras pruebas

--- app/src/main/java/com/cesarolivera/proyectofinal/view/MainViewModel.kt
+++ app/src/main/java/com/cesarolivera/proyectofinal/view/MainViewModel.kt
@@ -17,22 +17,4 @@ class MainViewModel: ViewModel() {
         val areEqual = text1 == text2
         _resultLiveData.value = if (areEqual) "Los textos son iguales." else "Los textos son diferentes."
     }
-    //val datos: LiveData<Datos> get() = _datos
-    //private var _datos = MutableLiveData<Datos>(Datos("","",""))
-
-    //val areTextsEqual = MutableLiveData<Boolean>()
-
-    //val datos = MutableLiveData(Datos("", "", ""))
-
-    /*fun compareTexts(text1: String, text2: String) {
-        areTextsEqual.value = text1 == text2
-    }
-    fun compareTexts(){
-        val text1 = _datos.value?.primerTexto ?: ""
-        val text2 = _datos.value?.segundoTexto ?: ""
-        val areEqual = text1 == text2
-        val resultMessage = if (areEqual) "Los textos son iguales." else "Los textos son diferentes."
-        println(resultMessage)
-        _datos.value?.resultTextView = resultMessage
-    }*/
 }
